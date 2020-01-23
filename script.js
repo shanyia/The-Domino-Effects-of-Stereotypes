4ce310c14d979260cb9f47d210a7276336e798e3
shanyia <60224968+shanyia@users.noreply.github.com>
2020-01-23 08:36:32 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $(".Suicide").hide();
+});
+
+$(".pic2").click(function() {
+    $(".Suicide").toggle();
+
+});
+
+$(document).ready(function() {
+    $(".bulling").hide();
+
+});
+
+$(".pic3").click(function() {
+    $(".bulling").toggle();
+
+});
+
+$(document).ready(function() {
+    $(".policebrutality").hide();
+});
+$(".pic4").click(function() {
+    $(".policebrutality").toggle();
+
+});
